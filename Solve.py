6e22e52a5c6ab661e80e2772fe3c0eddd7039507
Alexander Sukhikh <xbox24_772@mail.ru>
2020-10-18 20:17:30 +0300

    Finish first lab

--- Solve.py
+++ Solve.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot
 import csv
 
 
-class Plot:
+class AbstractPlot:
     def __init__(self, plot_name="", xname="", yname=""):
         self.title = plot_name
         self.xname = xname
@@ -16,15 +16,12 @@ class Plot:
 
     def add(self, x, y, label=""):
         self.plots.append((x, y, label))
-        self.plot.plot(x, y, label=label)
 
     def restore(self):
         self.clear()
         self.plot.title(self.title)
         self.plot.xlabel(self.xname)
         self.plot.ylabel(self.yname)
-        for plt in self.plots:
-            self.plot.plot(plt[0], plt[1], label=plt[2])
 
     def show(self):
         self.plot.legend()
@@ -39,6 +36,34 @@ class Plot:
         self.plot.clf()
 
 
+class Plot(AbstractPlot):
+    def __init__(self, plot_name="", xname="", yname=""):
+        super().__init__(plot_name, xname, yname)
+
+    def add(self, x, y, label=""):
+        super().add(x, y, label)
+        self.plot.plot(x, y, label=label)
+
+    def restore(self):
+        super().restore()
+        for plt in self.plots:
+            self.plot.plot(plt[0], plt[1], label=plt[2])
+
+
+class Scatter(AbstractPlot):
+    def __init__(self, plot_name="", xname="", yname=""):
+        super().__init__(plot_name, xname, yname)
+
+    def add(self, x, y, label=""):
+        super().add(x, y, label)
+        self.plot.scatter(x, y, label=label)
+
+    def restore(self):
+        super().restore()
+        for plt in self.plots:
+            self.plot.scatter(plt[0], plt[1], label=plt[2])
+
+
 class Solver:
     def __init__(self):
         self.rf_code = '643'
@@ -47,7 +72,7 @@ class Solver:
         self.both_file = "both.csv"
 
     @staticmethod
-    def process_data(processing_year_previous, processing_year_current, do_round=True):
+    def process_survival_data(processing_year_previous, processing_year_current, do_round=True):
         coefficients = [1]
         ages = [0]
         for i in range(1, len(processing_year_current)):
@@ -56,9 +81,9 @@ class Solver:
                 coefficients.append(coefficients[-1])
             else:
                 coefficients.append(processing_year_current[i] / processing_year_previous[i - 1])
-            if do_round:
-                coefficients = list(map(lambda x: round(x, 3), coefficients))
             ages.append(ages[-1] + 5)
+        if do_round:
+            coefficients = list(map(lambda x: round(x, 3), coefficients))
         return ages, coefficients
 
     @staticmethod
@@ -81,8 +106,8 @@ class Solver:
         males_2005 = self.get_row(self.males_file, self.rf_code, year)
         plot = Plot("survival rate", "age", "coeff")
 
-        ages, males_survival_2005 = self.process_data(males_2005, males_2000, do_round)
-        ages, females_survival_2005 = self.process_data(females_2005, females_2000, do_round)
+        ages, males_survival_2005 = self.process_survival_data(males_2005, males_2000, do_round)
+        ages, females_survival_2005 = self.process_survival_data(females_2005, females_2000, do_round)
 
         if do_plot:
             plot.add(ages, females_survival_2005, "females rate")
@@ -100,7 +125,7 @@ class Solver:
         # and so on: 10, 15 years ago and also in future: +5, +10, +15 years
         # so we can count fertility starting from 1965 to 1990
         females_cnt = []
-        fertility_years = range(1965, 1990, 5)
+        fertility_years = range(1965, 1995, 5)
         births = [0 for _ in range(len(fertility_years))]
         years = []
         for i, fertility_year in enumerate(fertility_years):
@@ -120,13 +145,13 @@ class Solver:
                              * (last_in - first_in) / 4
                 first_in += 1
 
-        print(females_cnt)
-        print(births)
+        # print(females_cnt)
+        # print(births)
         fertility = [x / y for x, y in zip(births, females_cnt)]
         if do_plot:
             plot = Plot("fertility rate", "years", "rate")
             plot.add(years, fertility)
-            plot.show()
+            # plot.show()
             plot.save("results/fertility_rate.png")
         return fertility
 
@@ -148,63 +173,94 @@ class Solver:
             plot = Plot("birth rate", "year", "males/females")
             plot.add(years, birth_rate)
             plot.save("results/birth_rate.png")
-            # del plot
-            # plot = Plot("birth rate", "year", "rate")
-            # plot.add(years, get_rate(males_birth, both_birth), "males/both")
-            # plot.add(years, get_rate(females_birth, both_birth), "females/both")
+            del plot
+            plot = Plot("birth rate", "year", "rate")
+            plot.add(years, get_rate(males_birth, both_birth), "males/both")
+            plot.add(years, get_rate(females_birth, both_birth), "females/both")
+            plot.save("results/birth_rate_gender.png")
             # plot.show()
         return birth_rate
 
-    def convert_coef(self):
+    def convert_coef(self, verbose=True):
         convert = lambda lst: [x ** 0.2 for x in lst]
         fertility = convert(self.fertility_rate(False))
-        # print(f"fertility coef:\n{fertility}")
         survival = self.survival_rate(do_plot=False, do_round=False)
         survival_man = convert(survival[1])
         survival_women = convert(survival[2])
         birth_rate = convert(self.birth_rate(False))
-        # print(f"survival:\nman: {survival_man}\nwomen: {survival_women}")
+        if verbose:
+            print(f"fertility coef:\n{fertility}")
+            print(f"survival:\nman: {survival_man}\nwomen: {survival_women}")
+            print(f"birth rate:\n{birth_rate}")
         return fertility[-1], birth_rate[-1], survival_man, survival_women
 
     def prognosis(self, do_plot=True):
-        fertility, birth_rate, survival_man, survival_women = self.convert_coef()
-        print(f"fertility: {fertility}\nbirth: {birth_rate}\nman: {survival_man}\nwomen: {survival_women}")
-        prognose_people = [list(map(lambda x: int(x/5), self.get_row(self.both_file, self.rf_code, '2005')))]
-        get_women_cnt = lambda all_people: birth_rate * all_people / (birth_rate + 1)
-        get_man_cnt = lambda all_people: all_people - get_women_cnt(all_people)
-        ages = range(5, 105, 5)
+        fertility, birth_rate, survival_man, survival_women = self.convert_coef(False)
+        group_cnt = 5
+        # print(f"fertility: {fertility}\nbirth: {birth_rate}\nman: {survival_man}\nwomen: {survival_women}")
+
+        def get_women_cnt(all_people):
+            return birth_rate * all_people / (birth_rate + 1)
+
+        def get_man_cnt(all_people):
+            return all_people - get_women_cnt(all_people)
+
+        def recalc_people(all_people, age):
+            return int(get_man_cnt(all_people) * survival_man[age // group_cnt] +
+                       get_women_cnt(all_people) * survival_women[age // group_cnt])
+
+        people_2005 = self.get_row(self.both_file, self.rf_code, '2005')
+        people_2005_by_year = []
+
+        # divide 5-age groups to one-age
+        for age in range(105):
+            if age % 5 == 0:
+                people_2005_by_year.append(people_2005[age//5])
+            else:
+                people_2005_by_year.append(recalc_people(people_2005_by_year[-1], age))
+
         years = range(2006, 2106)
+        fertility_start = 20
+        fertility_end = 40
+        prognosis_people = [people_2005_by_year]
         for _ in years:
-            previous_year = prognose_people[-1]
-            current_year_prognose = [0]
-            for i, age in enumerate(ages):
-                previous_year_people_previous_age = previous_year[age // 5 - 1]
-                current_year_prognose.append(int(
-                    get_man_cnt(previous_year_people_previous_age) * survival_man[i] +
-                    get_women_cnt(previous_year_people_previous_age) * survival_women[i]
-                ))
-            current_year_prognose[0] = int(get_women_cnt(sum(current_year_prognose[4:8])) * fertility)
-            prognose_people.append(current_year_prognose)
+            previous_year = prognosis_people[-1]
+            current_year_prognosis = [0]
+            for age in range(1, 105):
+                current_year_prognosis.append(recalc_people(previous_year[age], age))
+
+            current_year_prognosis[0] = int(get_women_cnt(
+                sum(current_year_prognosis[fertility_start:fertility_end])
+            ) * fertility)
+            prognosis_people.append(current_year_prognosis)
+
+        prognosis_people.pop(0)  # exclude 2005 people
+        age_groups = range(5, 106, 5)
+
+        # return 5-age groups
+        for age in range(len(prognosis_people)):
+            previous_group = 0
+            grouped_people = []
+            for group in age_groups:
+                grouped_people.append(sum(prognosis_people[age][previous_group: group]))
+                previous_group = group
+            prognosis_people[age] = grouped_people
 
         if do_plot:
             plot = Plot("russian population", "years", "people count")
-            plot.add(years, [sum(x) for x in prognose_people[1:]])
+            plot.add(years, [sum(x) for x in prognosis_people])
             plot.save("results/population100.png")
-            plot.show()
+            # plot.show()
             del plot
 
-            plot = Plot("survival rates", "ages", "people")
-            rates = []
-            for i in range(1, len(prognose_people)):
-                rates.append(self.process_data(prognose_people[i - 1], prognose_people[i]))
-
-            for i, year in enumerate(years):
-                if i % 10:
+            plot = Plot("Russia age profile", "ages", "people count")
+            for i, age in enumerate(years):
+                if (i+1) % 20:
                     continue
-                plot.add(*rates[i], f"people {year}")
-            plot.save("results/survival_rate_prognose.png")
-            plot.show()
-        return prognose_people
+                plot.add(age_groups, prognosis_people[i], f"people {age}")
+            plot.save("results/year_distribution_prognose.png")
+            # plot.show()
+        return prognosis_people
 
     def solve(self):
         solve_order = (self.survival_rate, self.fertility_rate, self.birth_rate, self.convert_coef, self.prognosis)
